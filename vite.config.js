ab457deb7df2d30016fa7563fc1a4d67eaf83cdc
SharatChandraDonepudi <dbsd.sharatchandra@klh.edu.in>
2026-06-19 11:12:47 +0530

    Fix base path for Vercel and GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+
+// Vercel automatically sets process.env.VERCEL to '1' when building your site
+const isVercel = process.env.VERCEL === '1';
 
 export default defineConfig({
-  base:"/Portfolio-React/",
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react()],
+  // If it's Vercel, use '/'. If it's GitHub Pages, use your repository name.
+  base: isVercel ? '/' : '/Portfolio-React/',
 })
